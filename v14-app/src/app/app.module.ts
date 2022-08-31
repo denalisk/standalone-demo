385c81488ee3fec57c4977f80a681bac67bd4d62
Sam Kirsch <samuelkirsch@microsoft.com>
2022-08-31 08:36:54 -0700

    add test standalone component

--- v14-app/src/app/app.module.ts
+++ v14-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StandaloneTestComponent } from './standalone-test/standalone-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StandaloneTestComponent
   ],
   imports: [
     BrowserModule,
---
+++ v14-app/src/app/standalone-test/standalone-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-standalone-test',
+  templateUrl: './standalone-test.component.html',
+  styleUrls: ['./standalone-test.component.less']
+})
+export class StandaloneTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
